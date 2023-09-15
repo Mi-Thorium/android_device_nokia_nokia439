422c6c8773c5e592e418e78be909921d6a024ea0
me-cafebabe <me.cafebabe@gmail.com>
2023-09-15 17:56:53 +0800

    nokia439: Update partition configuration
    * Referenced from AOSP docs and CAF msm8937_64 device tree.

--- device.mk
+++ device.mk
@@ -50,6 +50,38 @@ PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 #PRODUCT_COPY_FILES += \
 #    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_olive/android.hardware.fingerprint.xml
 
+# A/B
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_verifier
+
+# A/B - bootctrl
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service \
+    bootctrl.msm8937
+
+# A/B - Postinstallation - otapreopt
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# A/B - Postinstallation - Userdata checkpoint
+PRODUCT_PACKAGES += \
+    checkpoint_gc
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
