d4df79911cea0d30776685a54b3309fa8a07057b
me-cafebabe <me.cafebabe@gmail.com>
2023-09-16 01:09:38 +0800

    nokia439: Drop keylayout
    * Nokia SDM439 doesn't seem to have device specific keylayout

--- device.mk
+++ device.mk
@@ -105,11 +105,6 @@ endif
 #    android.hardware.biometrics.fingerprint@2.1-service.nokia_nokia439 \
 #    android.hardware.biometrics.fingerprint@2.2
 
-# Input
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
-
 # Recovery
 PRODUCT_COPY_FILES += \
     vendor/nokia/nokia439/proprietary/vendor/bin/hvdcp_opti:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/hvdcp_opti
