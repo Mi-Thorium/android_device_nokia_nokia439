be284f2c00824f7ce3b020e51cffa2d2571a8498
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-01 03:58:08 +0800

    [TEMP] SELinux Permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,7 +58,7 @@ TARGET_RECOVERY_DEVICE_MODULES := init_nokia_nokia439
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/non-existent androidboot.boot_devices=soc/7824900.sdhci
+BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/non-existent androidboot.boot_devices=soc/7824900.sdhci androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_RECOVERY_CONFIG := 
